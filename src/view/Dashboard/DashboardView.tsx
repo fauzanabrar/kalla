693e759dd06853a1f5f553fec56197ad43b9aeee
Fauzan Abrar <fauzanabrar24@gmail.com>
2023-09-09 18:58:01 +0800

    add zustand store

--- src/view/Dashboard/DashboardView.tsx
+++ src/view/Dashboard/DashboardView.tsx
@@ -1,22 +1,17 @@
 import React from "react";
-import CardBarChart from "../../components/Cards/CardBarChart";
-import CardLineChart from "../../components/Cards/CardLineChart";
-import CardPageVisits from "../../components/Cards/CardPageVisits";
-import CardSocialTraffic from "../../components/Cards/CardSocialTraffic";
-
-// components
 
 // layout for page
 import AdminWithStats from "../../layouts/MyLayouts/AdminWithStats";
 import { DataTableAsset } from "../../components/MyComponent/Shadcn/Table/TableAsset";
 
+
 export default function DashboardView() {
   return (
     <AdminWithStats>
-      <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-        <div className="px-4 py-5 flex-auto">
+      <div className="relative mb-8 flex w-full min-w-0 flex-col break-words rounded-lg bg-white shadow-lg">
+        <div className="flex-auto px-4 py-5">
           <DataTableAsset initialPageSize={5} tableName="Barang dan Jasa" />
-          </div>
+        </div>
       </div>
     </AdminWithStats>
   );
